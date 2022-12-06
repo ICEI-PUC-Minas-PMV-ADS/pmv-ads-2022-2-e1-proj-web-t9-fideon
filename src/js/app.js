4ac86dadaee266080cb6ab06380a4ec5ea65d87b
Diniz-Gustavo <116749525+Diniz-Gustavo@users.noreply.github.com>
2022-12-06 19:53:35 -0300

    json

--- src/js/app.js
+++ src/js/app.js
@@ -1,181 +1,4 @@
-var Cadastro = [
-    {
-        "nome":"Otávio das Neves",
-        "cidade":"Belo Horizonte",
-        "idade":20,
-        "email":"otavioneves@gmail.com",
-        "senha":"123456",
-        "telefone":"(31) 3081-6314",
-        "rank":1,
-        "favorito":"Padaria da Neca"
-    },
-    {
-        "nome":"Maria Luiza Aragão",
-        "cidade":"Belo Horizonte",
-        "idade":32,
-        "email":"malu.aragao@gmail.com",
-        "senha":"123456",
-        "telefone":"(31) 3033-0713",
-        "rank":2,
-        "favorito":"Padaria da Neca"
-    },
-    {
-        "nome":"Ana Pereira",
-        "cidade":"Belo Horizonte",
-        "idade":19,
-        "email":"pereira.ana@gmail.com",
-        "senha":"123456",
-        "telefone":"(31) 2389-4037",
-        "rank":3,
-        "favorito":"Padaria da Neca"
-    },
-    {
-        "nome":"Helena da Mata",
-        "cidade":"Belo Horizonte",
-        "idade":25,
-        "email":"helenamata12@gmail.com",
-        "senha":"123456",
-        "telefone":"(31) 3142-7280",
-        "rank":2,
-        "favorito":"Padaria da Neca"
-    },
-    {
-        "nome":"Heitor Oliveira",
-        "cidade":"Belo Horizonte",
-        "idade":41,
-        "email":"oliveiraheitor@gmail.com",
-        "senha":"123456",
-        "telefone":"(31) 2732-4117",
-        "rank":2,
-        "favorito":"Padaria da Neca"
-    },
-    {
-        "nome":"Raquel da Mota",
-        "cidade":"Belo Horizonte",
-        "idade":39,
-        "email":"raquelmota@gmail.com",
-        "senha":"123456",
-        "telefone":"(31) 2005-1896",
-        "rank":1,
-        "favorito":"Padaria da Neca"
-    },
-    {
-        "nome":"João Lucas Martins",
-        "cidade":"Belo Horizonte",
-        "idade":22,
-        "email":"lucasmartins.joao@gmail.com",
-        "senha":"123456",
-        "telefone":"(35) 2870-2070",
-        "rank":1,
-        "favorito":"Padaria da Neca"
-    },
-    {
-        "nome":"Carolina Moura",
-        "cidade":"Belo Horizonte",
-        "idade":23,
-        "email":"carolmou@gmail.com",
-        "senha":"123456",
-        "telefone":"(32) 2315-6615",
-        "rank":2,
-        "favorito":"Padaria da Neca"
-    },
-    {
-        "nome":"Davi Lucca Martins",
-        "cidade":"Belo Horizonte",
-        "idade":20,
-        "email":"davilucca@gmail.com",
-        "senha":"123456",
-        "telefone":"(32) 3706-7007",
-        "rank":3,
-        "favorito":"Padaria da Neca"
-    }
-];
 
-var Empresas = [
-    {
-        "nome":"Padaria da Neca",
-        "endereço":"Av dos Milagres, 982",
-        "bairro":"Cabral",
-        "telefone":"(31) 3234 5432",
-        "email":"padariadaneca@fideon.com.br",
-        "senha":"123456",
-        "comentarios":[
-            "Atendimento excelente! Preços ok",
-            "Tudo perfeito!",
-            "Achei os preços um pouco salgados, mas de resto, perfeito",
-            "Funciona 24h!"],
-        "vendas":[
-            "Pão Francês, 1000 unidades",
-            "Pizzas, 5 unidades"
-        ]
-    },
-    {
-        "nome":"Pizzaria do Doca",
-        "endereço":"Av Paulista, 321",
-        "bairro":"Dona Clara",
-        "telefone":"(31) 3122 8965",
-        "email":"pizzadodoca@fideon.com.br",
-        "senha":"123456",
-        "comentarios":[
-            "Otima pizza, pessimo atendimento",
-        ],
-        "vendas":[
-            "Pão Francês, 1000 unidades",
-            "Pizzas, 5 unidades"
-        ]
-    },
-    {
-        "nome":"Restaurante da Esquina",
-        "endereço":"Rua dos Beija-flores, 99",
-        "bairro":"Tiradentes",
-        "telefone":"(31) 3667 5489",
-        "email":"esquinarestaurante@fideon.com.br",
-        "senha":"123456",
-        "comentarios":[
-            "Ideal para um almoço executivo, sem gastar muito",
-        ],
-        "vendas":[
-            "Pão Francês, 1000 unidades",
-            "Pizzas, 5 unidades"
-        ]
-    },
-    {
-        "nome":"Bão de Minas",
-        "endereço":"Ruas dos Atleticanos, 299",
-        "bairro":"Caiçara",
-        "telefone":"(31) 3381-9638",
-        "email":"baodeminas@fideon.com.br",
-        "senha":"123456",
-        "comentarios":[
-            "Atendimento excelente! Preços de acordo com a qualidade do local",
-            "Tudo perfeito!",
-            "Local super aconchegante e familiar",
-            "Brunch maravilhoso, super recomendo!"
-        ],
-        "vendas":[
-            "Pão Francês, 1000 unidades",
-            "Pizzas, 5 unidades"
-        ]
-    },
-    {
-        "nome":"Açaí Legal",
-        "endereço":"Rua dos Cruzeirenses, 399 B",
-        "bairro":"Floresta",
-        "telefone":"(31) 2213-5642",
-        "email":"acailegal@fideon.com.br",
-        "senha":"123456",
-        "comentarios":[
-            "Atendimento excelente! Preços ok",
-            "Tudo perfeito!",
-            "Achei os preços um pouco salgados, mas de resto, perfeito",
-            "Funciona 24h!"
-        ],
-        "vendas":[
-            "Pão Francês, 1000 unidades",
-            "Pizzas, 5 unidades"
-        ]
-    }
-];
 
 /* VALIDAR LOGIN */
 
@@ -373,3 +196,181 @@ function ExibirComentarios(){
 };
 
 
+var Cadastro = [
+    {
+        "nome":"Otávio das Neves",
+        "cidade":"Belo Horizonte",
+        "idade":20,
+        "email":"otavioneves@gmail.com",
+        "senha":"123456",
+        "telefone":"(31) 3081-6314",
+        "rank":1,
+        "favorito":"Padaria da Neca"
+    },
+    {
+        "nome":"Maria Luiza Aragão",
+        "cidade":"Belo Horizonte",
+        "idade":32,
+        "email":"malu.aragao@gmail.com",
+        "senha":"123456",
+        "telefone":"(31) 3033-0713",
+        "rank":2,
+        "favorito":"Padaria da Neca"
+    },
+    {
+        "nome":"Ana Pereira",
+        "cidade":"Belo Horizonte",
+        "idade":19,
+        "email":"pereira.ana@gmail.com",
+        "senha":"123456",
+        "telefone":"(31) 2389-4037",
+        "rank":3,
+        "favorito":"Padaria da Neca"
+    },
+    {
+        "nome":"Helena da Mata",
+        "cidade":"Belo Horizonte",
+        "idade":25,
+        "email":"helenamata12@gmail.com",
+        "senha":"123456",
+        "telefone":"(31) 3142-7280",
+        "rank":2,
+        "favorito":"Padaria da Neca"
+    },
+    {
+        "nome":"Heitor Oliveira",
+        "cidade":"Belo Horizonte",
+        "idade":41,
+        "email":"oliveiraheitor@gmail.com",
+        "senha":"123456",
+        "telefone":"(31) 2732-4117",
+        "rank":2,
+        "favorito":"Padaria da Neca"
+    },
+    {
+        "nome":"Raquel da Mota",
+        "cidade":"Belo Horizonte",
+        "idade":39,
+        "email":"raquelmota@gmail.com",
+        "senha":"123456",
+        "telefone":"(31) 2005-1896",
+        "rank":1,
+        "favorito":"Padaria da Neca"
+    },
+    {
+        "nome":"João Lucas Martins",
+        "cidade":"Belo Horizonte",
+        "idade":22,
+        "email":"lucasmartins.joao@gmail.com",
+        "senha":"123456",
+        "telefone":"(35) 2870-2070",
+        "rank":1,
+        "favorito":"Padaria da Neca"
+    },
+    {
+        "nome":"Carolina Moura",
+        "cidade":"Belo Horizonte",
+        "idade":23,
+        "email":"carolmou@gmail.com",
+        "senha":"123456",
+        "telefone":"(32) 2315-6615",
+        "rank":2,
+        "favorito":"Padaria da Neca"
+    },
+    {
+        "nome":"Davi Lucca Martins",
+        "cidade":"Belo Horizonte",
+        "idade":20,
+        "email":"davilucca@gmail.com",
+        "senha":"123456",
+        "telefone":"(32) 3706-7007",
+        "rank":3,
+        "favorito":"Padaria da Neca"
+    }
+];
+
+var Empresas = [
+    {
+        "nome":"Padaria da Neca",
+        "endereço":"Av dos Milagres, 982",
+        "bairro":"Cabral",
+        "telefone":"(31) 3234 5432",
+        "email":"padariadaneca@fideon.com.br",
+        "senha":"123456",
+        "comentarios":[
+            "Atendimento excelente! Preços ok",
+            "Tudo perfeito!",
+            "Achei os preços um pouco salgados, mas de resto, perfeito",
+            "Funciona 24h!"],
+        "vendas":[
+            "Pão Francês, 1000 unidades",
+            "Pizzas, 5 unidades"
+        ]
+    },
+    {
+        "nome":"Pizzaria do Doca",
+        "endereço":"Av Paulista, 321",
+        "bairro":"Dona Clara",
+        "telefone":"(31) 3122 8965",
+        "email":"pizzadodoca@fideon.com.br",
+        "senha":"123456",
+        "comentarios":[
+            "Otima pizza, pessimo atendimento",
+        ],
+        "vendas":[
+            "Pão Francês, 1000 unidades",
+            "Pizzas, 5 unidades"
+        ]
+    },
+    {
+        "nome":"Restaurante da Esquina",
+        "endereço":"Rua dos Beija-flores, 99",
+        "bairro":"Tiradentes",
+        "telefone":"(31) 3667 5489",
+        "email":"esquinarestaurante@fideon.com.br",
+        "senha":"123456",
+        "comentarios":[
+            "Ideal para um almoço executivo, sem gastar muito",
+        ],
+        "vendas":[
+            "Pão Francês, 1000 unidades",
+            "Pizzas, 5 unidades"
+        ]
+    },
+    {
+        "nome":"Bão de Minas",
+        "endereço":"Ruas dos Atleticanos, 299",
+        "bairro":"Caiçara",
+        "telefone":"(31) 3381-9638",
+        "email":"baodeminas@fideon.com.br",
+        "senha":"123456",
+        "comentarios":[
+            "Atendimento excelente! Preços de acordo com a qualidade do local",
+            "Tudo perfeito!",
+            "Local super aconchegante e familiar",
+            "Brunch maravilhoso, super recomendo!"
+        ],
+        "vendas":[
+            "Pão Francês, 1000 unidades",
+            "Pizzas, 5 unidades"
+        ]
+    },
+    {
+        "nome":"Açaí Legal",
+        "endereço":"Rua dos Cruzeirenses, 399 B",
+        "bairro":"Floresta",
+        "telefone":"(31) 2213-5642",
+        "email":"acailegal@fideon.com.br",
+        "senha":"123456",
+        "comentarios":[
+            "Atendimento excelente! Preços ok",
+            "Tudo perfeito!",
+            "Achei os preços um pouco salgados, mas de resto, perfeito",
+            "Funciona 24h!"
+        ],
+        "vendas":[
+            "Pão Francês, 1000 unidades",
+            "Pizzas, 5 unidades"
+        ]
+    }
+];
