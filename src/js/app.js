149713423625ddcfe1bf3ae2c28cc6f8756733d1
Diniz-Gustavo <116749525+Diniz-Gustavo@users.noreply.github.com>
2022-12-12 22:05:46 -0300

    final

--- src/js/app.js
+++ src/js/app.js
@@ -1,101 +1,122 @@
 var Cadastro = [
     {
-        "Index":1,
         "nome":"Otávio das Neves",
         "cidade":"Contagem",
         "idade":20,
-        "email":"otavioneves@gmail.com",
         "telefone":"(31) 3081-6314",
         "favorito":"Padaria da Neca",
         "rank":1,
+    },
+    {
+        "nome":"Maria Luiza Aragão",
+        "cidade":"Belo Horizonte",
+        "idade":32,
+        "telefone":"(31) 3033-0713",
+        "favorito":"Pizzaria do Doca",
+        "rank":2,
+    },
+    {
+        "nome":"Ana Pereira",
+        "cidade":"Betim",
+        "idade":19,
+        "telefone":"(31) 2389-4037",
+        "favorito":"Restaurante da Esquina",
+        "rank":3,
+    },
+    {
+        "nome":"Helena da Mata",
+        "cidade":"Belo Horizonte",
+        "idade":25,
+        "telefone":"(31) 3142-7280",
+        "favorito":"Padaria da Neca",
+        "rank":2,
+    },
+    {
+        "nome":"Heitor Oliveira",
+        "cidade":"Belo Horizonte",
+        "idade":41,
+        "telefone":"(31) 2732-4117",
+        "favorito":"Restaurante da Esquina",
+        "rank":2,
+    },
+    {
+        "nome":"Raquel da Mota",
+        "cidade":"Ribeirão das Neves",
+        "idade":39,
+        "telefone":"(31) 2005-1896",
+        "favorito":"Bão de Minas",
+        "rank":1,
+    },
+    {
+        "nome":"João Lucas Martins",
+        "cidade":"Nova Lima",
+        "idade":22,
+        "telefone":"(35) 2870-2070",
+        "favorito":"Padaria da Neca",
+        "rank":1,
+    },
+    {
+        "nome":"Carolina Moura",
+        "cidade":"Belo Horizonte",
+        "idade":23,
+        "telefone":"(32) 2315-6615",
+        "favorito":"Pizzaria do Doca",
+        "rank":2,
+    },
+    {
+        "nome":"Davi Lucca Martins",
+        "cidade":"Nova Lima",
+        "idade":20,
+        "telefone":"(32) 3706-7007",
+        "favorito":"Açai Legal",
+        "rank":3,
+    }
+];
+
+var validaCliente = [
+    {
+        "Index":1,
+        "email":"otavioneves@gmail.com",
         "senha":"123456"
     },
     {
         "Index":2,
-        "nome":"Maria Luiza Aragão",
-        "cidade":"Belo Horizonte",
-        "idade":32,
         "email":"malu.aragao@gmail.com",
-        "telefone":"(31) 3033-0713",
-        "favorito":"Pizzaria do Doca",
-        "rank":2,
         "senha":"123456",
     },
     {
         "Index":3,
-        "nome":"Ana Pereira",
-        "cidade":"Betim",
-        "idade":19,
         "email":"pereira.ana@gmail.com",
-        "telefone":"(31) 2389-4037",
-        "favorito":"Restaurante da Esquina",
-        "rank":3,
         "senha":"123456",
     },
     {
         "Index":4,
-        "nome":"Helena da Mata",
-        "cidade":"Belo Horizonte",
-        "idade":25,
         "email":"helenamata12@gmail.com",
-        "telefone":"(31) 3142-7280",
-        "favorito":"Padaria da Neca",
-        "rank":2,
         "senha":"123456",
     },
     {
         "Index":5,
-        "nome":"Heitor Oliveira",
-        "cidade":"Belo Horizonte",
-        "idade":41,
         "email":"oliveiraheitor@gmail.com",
-        "telefone":"(31) 2732-4117",
-        "favorito":"Restaurante da Esquina",
-        "rank":2,
         "senha":"123456",
     },
     {
         "Index":6,
-        "nome":"Raquel da Mota",
-        "cidade":"Ribeirão das Neves",
-        "idade":39,
         "email":"raquelmota@gmail.com",
-        "telefone":"(31) 2005-1896",
-        "favorito":"Bão de Minas",
-        "rank":1,
         "senha":"123456",
     },
     {
         "Index":7,
-        "nome":"João Lucas Martins",
-        "cidade":"Nova Lima",
-        "idade":22,
         "email":"lucasmartins.joao@gmail.com",
-        "telefone":"(35) 2870-2070",
-        "favorito":"Padaria da Neca",
-        "rank":1,
         "senha":"123456",
     },
     {
         "Index":8,
-        "nome":"Carolina Moura",
-        "cidade":"Belo Horizonte",
-        "idade":23,
         "email":"carolmou@gmail.com",
-        "telefone":"(32) 2315-6615",
-        "favorito":"Pizzaria do Doca",
-        "rank":2,
         "senha":"123456",
     },
     {
         "Index":9,
-        "nome":"Davi Lucca Martins",
-        "cidade":"Nova Lima",
-        "idade":20,
         "email":"davilucca@gmail.com",
-        "telefone":"(32) 3706-7007",
-        "favorito":"Açai Legal",
-        "rank":3,
         "senha":"123456",
     }
 ];
@@ -109,7 +130,7 @@ var Adm = [
     }
 ]
 
-var ValidaEmpresas = [
+var validaEmpresas = [
     {
         "index":1,
         "email":"padariadaneca@fideon.com.br",
@@ -139,9 +160,7 @@ var ValidaEmpresas = [
         "email":"acailegal@fideon.com.br",
         "senha":"123456"
     }
-
-]
-
+];
 
 var Empresas = [
     {
@@ -153,7 +172,10 @@ var Empresas = [
             "Atendimento excelente! Preços ok",
             "Tudo perfeito!",
             "Achei os preços um pouco salgados, mas de resto, perfeito",
-            "Funciona 24h!"]
+            "Funciona 24h!"
+        ],
+        "vendas":6239.98,
+        "cliente":2,
     },
     {
         "nome":"Pizzaria do Doca",
@@ -162,7 +184,9 @@ var Empresas = [
         "telefone":"(31) 3122 8965",
         "comentarios":[
             "Otima pizza, pessimo atendimento",
-        ]
+        ],
+        "vendas":1121.44,
+        "cliente":7,
     },
     {
         "nome":"Restaurante da Esquina",
@@ -171,7 +195,9 @@ var Empresas = [
         "telefone":"(31) 3667 5489",
         "comentarios":[
             "Ideal para um almoço executivo, sem gastar muito",
-        ]
+        ],
+        "vendas":4221.10,
+        "cliente":3,
     },
     {
         "nome":"Bão de Minas",
@@ -183,7 +209,9 @@ var Empresas = [
             "Tudo perfeito!",
             "Local super aconchegante e familiar",
             "Brunch maravilhoso, super recomendo!"
-        ]
+        ],
+        "vendas":3211.22,
+        "cliente":1,
     },
     {
         "nome":"Açaí Legal",
@@ -194,7 +222,9 @@ var Empresas = [
             "Açai super gostoso, mas faltaram opções de acompanhamento",
             "Não fui atendido, não saberia avaliar!",
             "Açai de verdade"
-        ]
+        ],
+        "vendas":2391.10,
+        "cliente":5,
     }
 ];
 
@@ -206,16 +236,16 @@ function validarLogin(){
     var ClientePassword = document.getElementById("uniquePW").value;
 
     
-    for (let i = 0; i < Cadastro.length; i++){
-        if(uniqueCliente === Cadastro[i].email && ClientePassword === Cadastro[i].senha){            
-            window.location.href = 'index-cliente.html' + '#' + Cadastro[i].Index;
+    for (let i = 0; i < validaCliente.length; i++){
+        if(uniqueCliente === validaCliente[i].email && ClientePassword === validaCliente[i].senha){             
+            window.location.href = 'index-cliente.html' + '#' + validaCliente[i].Index;
             return ;
         }          
     }
 
-    for (let i = 0; i < ValidaEmpresas.length; i++){
-        if(uniqueCliente === ValidaEmpresas[i].email && ClientePassword === ValidaEmpresas[i].senha){            
-            window.location.href = 'index-empresa.html' + '#' + ValidaEmpresas[i].index;            
+    for (let i = 0; i < validaEmpresas.length; i++){
+        if(uniqueCliente === validaEmpresas[i].email && ClientePassword === validaEmpresas[i].senha){         
+            window.location.href = 'index-empresa.html' + '#' + validaEmpresas[i].index;            
             return;
         }  
     }
@@ -230,12 +260,12 @@ function WelcomeCliente(){
     var index = window.location.hash.substring(1);
     var textoHTML = 'Olá, '
     for (let i = 0; i < Cadastro.length; i++){
-        if(index == Cadastro[i].Index){
+        if(index == validaCliente[i].Index){
             textoHTML += Cadastro[i].nome;
         }
     }
 
-    textoHTML += '!!' + '<br>' + 'No menu acima, você pode visualizar seus dados. Fique a vontade.';
+    textoHTML += '!!' + '<br>' + 'No menu ao lado, você pode visualizar seus dados. Fique a vontade.';
     var textoHTML2 = '';
 
 
@@ -250,12 +280,12 @@ function WelcomeEmpresa(){
     var index = window.location.hash.substring(1);
     var textoHTML = 'Olá, '
     for (let i = 0; i < Empresas.length; i++){
-        if(index == ValidaEmpresas[i].index){
+        if(index == validaEmpresas[i].index){
             textoHTML += Empresas[i].nome;
         }
     }
 
-    textoHTML += '!!' + '<br>' + 'No menu acima, você pode visualizar seus dados. Fique a vontade.';
+    textoHTML += '!!' + '<br>' + 'No menu ao lado, você pode visualizar seus dados. Fique a vontade.';
     var textoHTML2 = '';
 
 
@@ -265,8 +295,8 @@ function WelcomeEmpresa(){
     tela2.innerHTML = textoHTML2;
 }
 
-function OndeEncontrar(){
-    
+function OndeEncontrar(){    
+
     var textoHTML = 'Atualmente, temos parceiros localizados nos seguintes bairros de Belo Horizonte:<br>';
     var textoHTML2 = '';
 
@@ -291,10 +321,9 @@ function ExibirFavorito(index){
     tela.innerHTML = textoHTML;
     var tela2 = document.getElementById('tela2');
     tela2.innerHTML = textoHTML2;
-    
 };
 
-function ExibirBeneficios(){
+function ExibirBeneficios(index){
 
     var textoHTML = 'Seu rank é ';
     var textoHTML2 = 'Atualmente, a FideOn está trabalhando em sistemas de ranqueamento.<br>' +
@@ -303,15 +332,15 @@ function ExibirBeneficios(){
     'Cliente Bronze: 10% desconto em toda a loja.<br>';
 
 
-    switch(Cadastro[0].rank){
+    switch(Cadastro[index].rank){
         case 1:
-            textoHTML += 'Ouro. Válido por um ano, a partir da aquisição';
+            textoHTML += 'Ouro';
             break;
         case 2:
-            textoHTML += 'Prata. Válido por 6 meses, a partir da aquisição';
+            textoHTML += 'Prata';
             break;
         case 3:
-            textoHTML += 'Bronze. Válido por 1 mês, a partir da aquisição';
+            textoHTML += 'Bronze';
             break;
         default:
             textoHTML += 'Invalido';
@@ -336,7 +365,7 @@ function ExibirPerfis(){
         textoHTML += Cadastro[i].nome + ' - ';
         textoHTML += Cadastro[i].cidade + ' - ';
         textoHTML += Cadastro[i].idade + ' anos - ';
-        textoHTML += Cadastro[i].email+ ' - ';
+        textoHTML += validaCliente[i].email+ ' - ';
         textoHTML += Cadastro[i].telefone;        
         textoHTML += '<br>'
     };
@@ -365,17 +394,23 @@ function ExibirEmpresas(selector) {
 
 function ExibirCadastros(selector) {
 
+
+    var textoHTML = 'Atualmente, nosso banco de dados consta com os seguintes clientes:<br><br>';
+
     var columns = addTableHeaderCadastros(Cadastro, selector);
   
     for (var i = 0; i < Cadastro.length; i++) {
       var row$ = $('<tr/>');
-      for (var colIndex = 0; colIndex < columns.length - 1; colIndex++) {
+      for (var colIndex = 0; colIndex < columns.length; colIndex++) {
         var cellValue = Cadastro[i][columns[colIndex]];
         if (cellValue == null) cellValue = "";
         row$.append($('<td/>').html(cellValue));
       }
       $(selector).append(row$);
     }
+
+    var tela = document.getElementById('tela');
+    tela.innerHTML = textoHTML;
 };
   
   // Adds a header row to the table and returns the set of columns.
@@ -425,7 +460,6 @@ function ExibirRank(){
     var textoHTML = 'Os clientes, em ordem de ranqueamento, são:<br>';
     var textoHTML2 = '';
 
-
     for ( let j = 1; j <= 3; j++){            
         for ( let i = 0; i < Cadastro.length; i++){
                 if(Cadastro[i].rank == j){
@@ -449,29 +483,62 @@ function ExibirRank(){
     tela2.innerHTML = textoHTML2;
 };
 
-
-
-function ExibirComentarios(){
-
-    var index = window.location.hash.substring(1);
+function meuPerfil(index){
+    var index  = window.location.hash.substring(1) - 1;
     var textoHTML = '';
     var textoHTML2 = '';
 
-        textoHTML += '<br><br><br>'
-        textoHTML += Empresas[index].nome;
-        for(let j = 0; j<Empresas[index].comentarios.length; j++)
-        {
-            textoHTML += '<br><br>'
-            textoHTML += Empresas[index].comentarios[j] + ' - ';
-        };
+    textoHTML += Empresas[index].nome + '<br>';
+    textoHTML += Empresas[index].endereço + '<br>';
+    textoHTML += Empresas[index].bairro + '<br>';
+    textoHTML += Empresas[index].telefone;
+
+    var tela = document.getElementById('tela');
+    tela.innerHTML = textoHTML;
+    var tela2 = document.getElementById('tela2');
+    tela2.innerHTML = textoHTML2;     
+}
+
+//daqui pra baixo, Junia diz que tentou fazer algo kkkk
+
+function ExibirComentarios(index){
+
+    var index  = window.location.hash.substring(1) - 1;
+    var textoHTML = '';
+    var textoHTML2 = '';
+    textoHTML += '<br><br><br>'
+    textoHTML += Empresas[index].nome + '<br>';
+    for(let j = 0; j<Empresas[index].comentarios.length; j++)
+    {
+        textoHTML += '<br><br>'
+        textoHTML += Empresas[index].comentarios[j] + '<br>';
+    };
 
     var tela = document.getElementById('tela');
     tela.innerHTML = textoHTML;
     var tela2 = document.getElementById('tela2');
     tela2.innerHTML = textoHTML2;
+    
+    return index;
 };
 
+function Relatorio(index){
 
+    var index  = window.location.hash.substring(1) - 1;
+    var textoHTML = '';
+    var textoHTML2 = '';
 
+    textoHTML += 'Foram gerados para voce, ' + Empresas[index].nome +', R$' + Empresas[index].vendas + ' oriundos de clientes FideOn';
+    textoHTML += '<br>O cliente do mês foi ';
+    
+    for(var i = 1; i < Cadastro.length; i++){
+        if(Empresas[index].cliente == validaCliente[i].Index){
+            textoHTML += Cadastro[i].nome;
+        }
+    };
 
-
+    var tela = document.getElementById('tela');
+    tela.innerHTML = textoHTML;
+    var tela2 = document.getElementById('tela2');
+    tela2.innerHTML = textoHTML2;
+}
